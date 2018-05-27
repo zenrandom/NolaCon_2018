3e34506a2805ebfa6b6248727e4e770b4e23c2b2
thedevilsvoice <franklin@bitsmasher.net>
2018-05-27 10:16:44 -0400

    fix display height

--- arduino/hardware_test/deadlocks.h
+++ arduino/hardware_test/deadlocks.h
@@ -7,7 +7,7 @@
 Adafruit_SSD1306 display(OLED_RESET);
 
 #if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#warning("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
 /*
--- arduino/nola_con/nola_con.ino
+++ arduino/nola_con/nola_con.ino
@@ -31,10 +31,11 @@ MyGame mygame;
 #define OLED_RESET 2
 Adafruit_SSD1306 display(OLED_RESET);
 
-//#if (SSD1306_LCDHEIGHT != 64)
-//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-//#endif
-//#define SSD1306_LCDHEIGHT = 64
+#if (SSD1306_LCDHEIGHT != 64)
+#warning("Height incorrect, please fix Adafruit_SSD1306.h!");
+#define SSD1306_LCDHEIGHT = 64
+#endif
+
 
 // ThreadController that will controll all threads
 ThreadController controll = ThreadController();
